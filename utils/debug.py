2fc379d69acc1a00d56ffab5bb1fe409504887e3
mattarm555 <83235730+mattarm555@users.noreply.github.com>
2025-04-10 18:59:19 -0400

    Add files via upload

--- utils/debug.py
+++ utils/debug.py
@@ -1,1 +1,8 @@
+# utils/debug.py
 
+def debug_command(command_name, user, **kwargs):
+    print(f"\033[1;32m[COMMAND] /{command_name}\033[0m triggered by \033[1;33m{user.display_name}\033[0m")
+    if kwargs:
+        print("\033[1;36mInput:\033[0m")
+        for key, value in kwargs.items():
+            print(f"\033[1;31m  {key.capitalize()}: {value}\033[0m")
